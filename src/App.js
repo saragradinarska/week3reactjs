2684efac5a91f6afbb5356463e3666b84ad59e4a
Sara Gradinarska <saragradinarska@Saras-MacBook-Air-8.local>
2019-09-24 18:12:47 +0800

    made changes

--- src/App.js
+++ src/App.js
@@ -5,20 +5,52 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Analyzing Online Bullying Through the Stack Theory</h1>
+        <h1>Stack Layer One: User</h1>
+
+   
+    <img src="https://icdn5.digitaltrends.com/image/digitaltrends/bitmoji_feature_11-500x300-c.png" alt="Digital version of me as the User interacting with the social media platform and experiencing bullying" width="640" height="480" />
+       
+    
+    <div class= "para1">
+    <p>Through the stack Theory and its 6 layers I will analyze a negative aspect of life in a digital world. In the context of the issue of online bullying brought about by the rapid development of technology, I am the User. I have noticed throughout my youth the immense effect that social media and in particular, Instagram, has on the degree of self-esteem and sense of self-worth in young adults. Though one may argue that if we turn off social media and avoid following accounts that encourage unrealistic beauty standards, we would be safe from the harmful effects of Instagram. However, I would argue that social media is an essential part of our connectedness to the world and it is considerably hard, if not impossible, to distance oneself from the public scrutiny of one's peers.</p>
+       </div> 
+
+    <h1>Stack Layer Two: Interface</h1>
+        <img src="https://blogging.com/wp-content/uploads/2017/11/Instagram-6.png" alt="Social media platform Instagram as the arena for bullying and dissemination of hate among young adults" width="640" height="480"/>
+        
+        <div class= "para2">
+        <p>The Interface layer is exemplified through the social media platform Instagram on which the bullying occurs. In particular it is the iOS APP OF instagram that can be installed on all Apple and Android devices from their app stores, or in Android's case, from the Google Play Store. </p>
+    </div>
+        
+        <h1>Stack Layer Three: Address</h1>
+        <img src="https://gadget.co.za/wp-content/uploads/2018/11/after-data-breach-770x494.jpg" alt="The local network or User IP address is the address where the bullying is facilitated" width="640" height="480"/>
+        
+        <div class= "para3">
+        <p>The Address layer relates to the IP address of the website Instagram. It can also be perceived as the local network through which the interface (Instagram) is accessed by the User (me) or other young adults. This is mainly linked to the wifi routers that are omnipresent in my workplace, university and home. Although facilitating an ease of connectivity and communication among friends thousands of miles away, the wireless Internet access has brought about an interesting phenomenon. It is almost impossible to avoid the glow of our device screens, we are drawn to it, and in some cases, prefer the company of technology over that of people, due to the lack of communication skills necessary to type up a message online. This, however, also leads to increased anonimity of online trolls and bullies and the rapid spread of hateful messages and comments.</p>
+    </div>
+        
+        <h1>Stack Layer Four: City</h1>
+        <img src="https://tr1.cbsistatic.com/hub/i/2018/08/10/4eed15a6-7cf3-48ca-906a-09bab5e54ecd/marino1.jpg" alt="" sidth="640" height="480" />
+        
+        <div class= "para4">
+        <p>The fourth layer in the Stack theory is that of the City. I believe this layer is exemplified through the various wires that run throughout the city and feed on electricity, as well as the data centers that store all User information and act as hubs of data. It can also be understood as the internet service provider we use, in my case this is China Mobile while I reside in China and TMobile while I reside in the United States.</p>
+        </div>
+        
+        <h1>Stack Layer Five: Cloud</h1>
+        <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/01/29/21/pg-14-cloud-rex.jpg?w968h681" alt=""width="640" height="480"/>
+      
+        <div class= "para5">
+        <p>The fifth layer of the Stack Theory in relation to the issue of online bullying and based on my personal experiences would be best summarized as the Cloud platform of information storage. It is the place where Instagram's servers connect, interact and store information for future use.</p>
+    </div>
+        
+        <h1>Stack Layer Six: Earth</h1>
+        <img src="https://www.telekom.com/resource/image/504076/landscape_ratio4x3/1296/972/e4cfaf2da445dcf4e8764bb80e15693a/eq/bi-170921-ngena-en.jpg" alt=""width="640" height="480"/>
+       
+        <div class= "para6">
+     <p>The sixth and final layer of the Stack is the Earth. To me, this layer consists of the web of people all over the world that have access to the Interface i.e, Instagram and the complex web of connectedness that a global Internet network provides.</p>
+    </div>
+    
     </div>
   );
 }
